00562808150e2787ee8d84033f54e8f7f5dd309e
KotaKato <katokota1102@icloud.com>
2020-09-19 12:06:29 +0900

    Change

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"strconv"
+	// "strconv"
 
 	// サブ
 	"./rssLib"
@@ -27,9 +27,11 @@ func main(){
     // URLを読む
     flag.Parse()
     url := flag.Arg(0)
+    // テスト用(URLの指定がないとNHKを利用する)
     if url == "" {
         url = "https://www.nhk.or.jp/r-news/podcast/nhkradionews.xml"
     }
+
     // RSSの取得
     podRssData := getRssData(url)
 
@@ -39,26 +41,49 @@ func main(){
 func window(paraData rssLib.RssData, url string) {
     // 表示のズレを防止
     runewidth.DefaultCondition.EastAsianWidth = false
-    infoStr := "Infomation about the " + url + "\n" +
-               "\n" +
-               "Title     : " + paraData.Channel.Title + "\n" +
-               "CopyRight : " + paraData.Channel.CopyRight + "\n" +
-               "Category  : " + paraData.Channel.ItunesCategory.Text + "\n" +
-               "\n" +
-               strconv.Itoa(len(paraData.Channel.Item)) + "話のエピソードがあります"
 
-    app := tview.NewApplication()
-    textView := tview.NewTextView().
-        SetRegions(true).
-        SetChangedFunc(func() {
-            app.Draw()
-        })
-    go func() {
-        fmt.Fprintf(textView, "%s", infoStr)
-    }()
-    textView.SetBorder(true)
-    if err := app.SetRoot(textView, true).SetFocus(textView).Run(); err != nil {
-        panic(err)
+    // tview.TextViewを返す
+    newPrimitive := func(text string) tview.Primitive {
+        return tview.NewTextView().
+            SetTextAlign(tview.AlignCenter).
+            SetText(text)
     }
+    menu    := newPrimitive("Menu")
+    main    := newPrimitive("Main content")
+
+    grid := tview.NewGrid().
+        SetBorders(true).
+        AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false)
+
+    grid.AddItem(menu, 1, 0, 1, 1, 0, 100, false).
+        AddItem(main, 1, 1, 1, 1, 0, 100, false)
+
+
+    if err := tview.NewApplication().SetRoot(grid, true).SetFocus(grid).Run(); err != nil {
+		panic(err)
+	}
+
+    // infoStr := "Infomation about the " + url + "\n" +
+    //            "\n" +
+    //            "Title     : " + paraData.Channel.Title + "\n" +
+    //            "CopyRight : " + paraData.Channel.CopyRight + "\n" +
+    //            "Category  : " + paraData.Channel.ItunesCategory.Text + "\n" +
+    //            "\n" +
+    //            strconv.Itoa(len(paraData.Channel.Item)) + "話のエピソードがあります"
+
+    // app := tview.NewApplication()
+    // textView := tview.NewTextView().
+    //     SetRegions(true).
+    //     SetChangedFunc(func() {
+    //         app.Draw()
+    //     })
+    // go func() {
+    //     fmt.Fprintf(textView, "%s", infoStr)
+    // }()
+    // textView.SetBorder(true)
+    // if err := app.SetRoot(textView, true).SetFocus(textView).Run(); err != nil {
+    //     panic(err)
+    // }
+
 }
 
